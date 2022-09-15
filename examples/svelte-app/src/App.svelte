ff4a9b52725c1424f9e68e0cc76d536cec256dec
KPidS <WKPidS@gmail.com>
2022-09-16 02:06:59 +0300

    feat: save fullscreen state

--- examples/svelte-app/src/App.svelte
+++ examples/svelte-app/src/App.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
+
+	import { getCurrent } from '@tauri-apps/api/window'
+
+	// import { getCurrent } from 
 	export let name: string;
+
+	const webview = getCurrent()
+
+	const handleFullscreen = async () => {
+		await webview.setFullscreen(!(await webview.isFullscreen()))
+	}
+	
+	const handleClose = async () => {
+		await webview.close()
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={handleFullscreen}>Swtich Fullscreen</button>
+	<button on:click={handleClose}>Close</button>
 </main>
 
 <style>
