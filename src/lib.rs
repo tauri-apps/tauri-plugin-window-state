9c36453097d79b7aed8ac9d51564609551ee1d97
armand-colin <56818352+armand-colin@users.noreply.github.com>
2022-10-11 22:42:05 +0200

    switched order of `set_size` and `set_position`
    Switched `set_size` and `set_position`, because it makes the window move slightly after each reopening if it has been resized

--- src/lib.rs
+++ src/lib.rs
@@ -80,6 +80,11 @@ impl<R: Runtime> WindowExt for Window<R> {
     if let Some(state) = c.get(self.label()) {
       self.set_decorations(state.decorated)?;
 
+      self.set_size(Size::Physical(PhysicalSize {
+        width: state.width,
+        height: state.height,
+      }))?;
+      
       let mut pos: Option<(i32, i32)> = None;
       for m in self.available_monitors()? {
         if m.name().map(ToString::to_string).unwrap_or_default() == state.monitor {
@@ -100,10 +105,6 @@ impl<R: Runtime> WindowExt for Window<R> {
       };
       self.set_position(Position::Physical(PhysicalPosition { x, y }))?;
 
-      self.set_size(Size::Physical(PhysicalSize {
-        width: state.width,
-        height: state.height,
-      }))?;
       if state.maximized {
         self.maximize()?;
       }
